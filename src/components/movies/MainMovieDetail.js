a36e441b64cf876baa03582346416d761184dc37
Brandon <brandon121j@gmail.com>
2021-11-18 12:53:16 -0500

    Minor css changes

--- src/components/movies/MainMovieDetail.js
+++ src/components/movies/MainMovieDetail.js
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Loading from './Loading';
 import jwt from 'jsonwebtoken';
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 require('dotenv').config();
 const api = process.env.REACT_APP_API_KEY;
@@ -23,26 +23,27 @@ export const MainMovieDetail = () => {
 		fetchMovieDetails();
 	}, []);
 
-    const notifyFailed = (input) => toast.error(`${input}`, {
-        position: "top-center",
-        autoClose: 4000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-    });
+	const notifyFailed = (input) =>
+		toast.error(`${input}`, {
+			position: 'top-center',
+			autoClose: 4000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		});
 
-    const notifySuccess = () => toast.success('Movie added to favorite!', {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-    });
-
+	const notifySuccess = () =>
+		toast.success('Movie added to favorite!', {
+			position: 'top-center',
+			autoClose: 3000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		});
 
 	async function fetchMovieDetails() {
 		try {
@@ -51,59 +52,78 @@ export const MainMovieDetail = () => {
 				`https://www.omdbapi.com/?apikey=${api}&i=${imdbID}&type=movie`
 			);
 			setTitle(result.data.Title);
-			setPoster(result.data.Poster)
+			setPoster(result.data.Poster);
 			setPlot(result.data.Plot);
 			setActors(result.data.Actors);
 			setDirector(result.data.Director);
 			setGenre(result.data.Genre);
 			setRelease(result.data.Released);
-			console.log(result)
+			console.log(result);
 		} catch (e) {
 			console.log(e);
 		}
 	}
 
-    async function addToFavorites() {
-        if (localStorage.getItem('loginToken') === null) {
-            notifyFailed('Please login')
-        } else {
-            let decodedToken = jwt.verify(localStorage.getItem('loginToken'), process.env.REACT_APP_JWT_SECRET);
+	async function addToFavorites() {
+		if (localStorage.getItem('loginToken') === null) {
+			notifyFailed('Please login');
+		} else {
+			let decodedToken = jwt.verify(
+				localStorage.getItem('loginToken'),
+				process.env.REACT_APP_JWT_SECRET
+			);
 
-            try {
-                let payload = await axios.post("http://localhost:3001/movies/add-favorite", {
-                    title: title,
-                    moviePoster: poster,
-                    imdbLink: `https://www.omdbapi.com/?apikey=${api}&i=${imdbID}`,
-                    userID: decodedToken.userID
-                },
-                {headers : {"Authorization" : `Bearer ${localStorage.getItem('loginToken')}`}})
-                console.log(payload)
-                notifySuccess('Movie added to favorites')
-            } catch(e) {
-                console.log(e)
-                notifyFailed(e.response.Error)
-            }
-        }
-    }
+			try {
+				let payload = await axios.post(
+					'http://localhost:3001/movies/add-favorite',
+					{
+						title: title,
+						moviePoster: poster,
+						imdbLink: `https://www.omdbapi.com/?apikey=${api}&i=${imdbID}`,
+						userID: decodedToken.userID,
+					},
+					{
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem('loginToken')}`,
+						},
+					}
+				);
+				console.log(payload);
+				notifySuccess('Movie added to favorites');
+			} catch (e) {
+				console.log(e);
+				notifyFailed(e.response.Error);
+			}
+		}
+	}
 
 	return (
-	<div className="SearchContainer">
-		<div className="SearchContainerBox">
-		<h1>{title}</h1>
-		<img alt={title} src={poster}/>
-		<h3 id='plot'>Plot: <br/>{plot}</h3>
-		<h3>Actors: <br/>{actors}</h3>
-		<h3>Director: {director}</h3>
-		<h3>Genre: {genre}</h3>
-		<h3>Release Date: {release}</h3>
-        <button style={{ cursor: 'pointer' }} onClick={addToFavorites}>Add Favorite</button>
-		<Link to='/'>
-		<button style={{ cursor: 'pointer' }}>Go Back</button>
-		</Link>
+		<div className="SearchContainer">
+			<div className="SearchContainerBox">
+				<h1>{title}</h1>
+				<img alt={title} src={poster} />
+				<h3 id="plot">
+					Plot: <br />
+					{plot}
+				</h3>
+				<h3>
+					Actors: <br />
+					{actors}
+				</h3>
+				<h3>Director: {director}</h3>
+				<h3>Genre: {genre}</h3>
+				<h3>Release Date: {release}</h3>
+                <button id='favoriteButton' style={{ cursor: 'pointer' }} onClick={addToFavorites}>
+						Add Favorite
+					</button>
+				<div className="buttonContainer">
+					<Link to="/">
+						<button style={{ cursor: 'pointer' }}>Go Back</button>
+					</Link>
+				</div>
+			</div>
 		</div>
-	</div>
-	)
-	
+	);
 };
 
 // export default MainMovieDetail;
